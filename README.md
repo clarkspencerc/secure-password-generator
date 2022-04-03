899142850e33cd8d958f2c74c3ba74269ed1d58d
clarkspencerc <clarkspencerc@gmail.com>
2022-04-02 22:38:59 -0600

    added arrays for alphabet, numbers, and special characters

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Password Generator Starter Code
+
+
+
+# Deployed Webiste 
+https://clarkspencerc.github.io/secure-password-generator/
+
+# resources 
+* auto filled in arrays learned from : "https://javascript.plainenglish.io/create-an-array-of-alphabet-characters-in-javascript-with-this-simple-trick-930033079dd3"
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
-// Assignment code here
+// define variabels that can be used to generate password 
+const alpha = Array.from(Array(26)).map((e, i) => i + 65) ;
+const alphabet = alpha.map((x)=> String.fromCharCode(x));
+const numbers = Array.from(Array(10).keys()); 
+var special = " !#$%& '()*+,-./:;<=>?@[\]^_`{|}~"; 
+const specials = Array.from(special); 
 
+console.log(numbers);
+console.log(alphabet);
+console.log(specials);
+
+// used to know how many characters should each critera should be allotted 
+//var customizations = 
+
+// random number generator that takes in a min and max varible this will be used to pull from the array
+
+// Assignment code here
+var generatePassword = function(){
+  console.log("generate password function works")
+  // select length 
+  // add + 1 to customizations when the user responds with YES to any of the criteria below
+  // would you like lowercase use make lower to ensure charactes are lowercase 
+  // would you like uppercase use make upper to ensure characters are uppercase 
+  // would you like numeric values 
+  // would you like special characters 
+
+  // take length and devide it by customizations so you can generate aproprietly (maybe this is too complicated)
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +36,17 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+console.log("write password function works & this button works");
+return password;  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// series of prompts 
+// set variables that will be user input 
+// select length min 8 max 128 
+// asked to include character types: Lowercase, Uppercase, numberic, and/or special characters 
+// validate selected input ; prompt again if no character type was selected 
+// once all prompts are answered then a password will generate that matches the selected criteria 
+// display password in an alert or write it to the page. 
